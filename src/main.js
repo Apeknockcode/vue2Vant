47b4dc934f6875cc906126fa43326103d463395a
陶健 <freeman_top@163.com>
2021-10-24 16:12:51 +0800

    新增封装请求

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import api from "@/tools/api/index"; //引入接口文件
+
+Vue.prototype.$Api = api;
 
 Vue.config.productionTip = false;
 
